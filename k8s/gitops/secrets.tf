07921d3cf8bfd138bb8bad1452ffc6037e6fffe7
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:14:18 +0300

    Refactor comments in secrets.tf for clarity

--- k8s/gitops/secrets.tf
+++ k8s/gitops/secrets.tf
@@ -1,12 +1,11 @@
 # --- Postgres Credentials in Secret Manager ---
-# 1. Generate a random password (optional, but good practice)
+# Generate a random password 
 resource "random_password" "db_password" {
   length           = 16
   special          = true
   override_special = "!#$%&*()"
 }
 
-# 2. Create the Secret
 resource "google_secret_manager_secret" "postgres_creds" {
   secret_id = "SECRET_ID"
   labels = {
@@ -17,7 +16,7 @@ resource "google_secret_manager_secret" "postgres_creds" {
   }
 }
 
-# 3. Add the initial secret version (JSON format for easy parsing)
+# Add the initial secret version (JSON format for easy parsing)
 resource "google_secret_manager_secret_version" "postgres_creds_version" {
   secret = google_secret_manager_secret.postgres_creds.id
   secret_data = jsonencode({
