e5694b1812bd68686dd0b15e37d2f3bb711f6c20
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:21:17 +0300

    Refactor firewall rules in firewallrules.tf

--- k8s/gke/firewallrules.tf
+++ k8s/gke/firewallrules.tf
@@ -1,9 +1,7 @@
-
-# Firewall rule to allow internal traffic within the VPC
 resource "google_compute_firewall" "allow_internal" {
   name          = "allow-internal-gke-vpc"
   network       = google_compute_network.vpc.self_link
-  source_ranges = [var.vpc_cidr] # Allow all traffic within the VPC
+  source_ranges = [var.vpc_cidr] 
   allow {
     protocol = "icmp"
   }
@@ -23,7 +21,6 @@ resource "google_compute_firewall" "allow_ssh_to_bastion" {
   name    = "allow-ssh-to-bastion"
   network = google_compute_network.vpc.self_link
 
-  # Allow TCP traffic on port 22 (SSH)
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -33,6 +30,5 @@ resource "google_compute_firewall" "allow_ssh_to_bastion" {
   # Example: source_ranges = ["YOUR_HOME_OR_OFFICE_IP/32"]
   source_ranges = ["0.0.0.0/0"]
 
-  # Apply this rule only to instances with the 'allow-ssh' tag
   target_tags = ["allow-ssh"]
 }
