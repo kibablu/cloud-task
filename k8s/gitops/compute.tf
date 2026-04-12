6dbf027eb8a6874b29caf9c6328b97407e6030de
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:07:25 +0300

    Refactor compute.tf by cleaning up comments
    Removed comments and adjusted the static IP resource block.

--- k8s/gitops/compute.tf
+++ k8s/gitops/compute.tf
@@ -4,20 +4,17 @@ resource "google_compute_global_address" "ingress_static_ip" {
 }
 
 # --- Bastion/Management VM with Static Public IP ---
-# 1. Reserve the VM's static IP
 resource "google_compute_address" "vm_static_ip" {
   name   = "chris-vm-static-ip"
   region = var.region
 }
 
-# 2. Create the VM
 resource "google_compute_instance" "bastion_vm" {
   name         = "chris-bastion-vm"
   machine_type = "e2-micro"
   zone         = var.zone
   tags         = ["bastion-host"]
 
-  # Boot disk setup
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
