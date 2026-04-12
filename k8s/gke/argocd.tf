727cafc04dc9fc1a285f0f56fded7ee1cb05e931
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:20:08 +0300

    Refactor ArgoCD GKE cluster Terraform configuration
    Removed comments and cleaned up the ArgoCD GKE cluster configuration.

--- k8s/gke/argocd.tf
+++ k8s/gke/argocd.tf
@@ -1,40 +1,29 @@
-# ------------------------------------------------
-# ArgoCD GKE Cluster
-# ------------------------------------------------
 resource "google_container_cluster" "argocd_cluster" {
   name     = "gke-argocd-cluster"
-  location = "${var.gcp_region}-b" # Using a single zone
+  location = "${var.gcp_region}-b" 
 
-  # Allow deletion of the cluster through Terraform
   deletion_protection = false
 
-  # Initial node count for default node pool
   initial_node_count = 1
   release_channel { channel = var.gke_release_channel }
 
-  # Network/Subnet configuration
   network    = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.argocd_subnet.self_link
 
   # Use default node pool and service account
   node_config {
-    # Machine type
     machine_type = "e2-medium"
 
-    # Disk configuration
     disk_type    = "pd-balanced"
     disk_size_gb = 20
 
-    # Use default service account
     service_account = "default"
 
-    # Use default OAuth scopes
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 
-  # IP allocation policy for VPC-native cluster
   ip_allocation_policy {
     cluster_secondary_range_name  = google_compute_subnetwork.argocd_subnet.secondary_ip_range[0].range_name
     services_secondary_range_name = google_compute_subnetwork.argocd_subnet.secondary_ip_range[1].range_name
@@ -71,9 +60,6 @@ resource "google_container_cluster" "argocd_cluster" {
 
 }
 
-# ------------------------------------------------
-# Helm Release for ArgoCD
-# ------------------------------------------------
 resource "helm_release" "argocd" {
   provider = helm.argocd_gke
 
