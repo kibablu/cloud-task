45a10534a4d7cde209be016e12825ed5795e494b
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:55:30 +0300

    Remove comment on random provider in provider.tf
    Removed comment about adding the random provider.

--- vms/wooecom/provider.tf
+++ vms/wooecom/provider.tf
@@ -1,11 +1,9 @@
-# Define required providers and set up configuration
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "6.44.0"
     }
-    # Adding the random provider for password generation
     random = { 
       source  = "hashicorp/random"
       version = "~> 3.0"
