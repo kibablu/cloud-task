5634f6b8251175889556a6fe77c5f671ca256ded
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:15:20 +0300

    removed typos

--- k8s/gitops/variables.tf
+++ k8s/gitops/variables.tf
@@ -1,4 +1,3 @@
-## Project and Location Variables
 variable "project_id" {
   description = "The GCP project ID to deploy resources into"
   type        = string
@@ -17,14 +16,12 @@ variable "zone" {
   default     = "us-central1-a"
 }
 
-## Domain Variables
 variable "domain_name" {
   description = "The public domain name for your Cloud DNS zone (e.g., chrisproject.org)"
   type        = string
-  default     = "klaudmazoezi.top"
+  default     = "example.com"
 }
 
-## GKE Cluster Variables
 variable "gke_node_machine_type" {
   description = "Machine type for the GKE nodes"
   type        = string
