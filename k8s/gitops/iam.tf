900b7d5572e06493f21298a315a4a725ee5dbe8d
kibablu <50038533+kibablu@users.noreply.github.com>
2026-04-12 07:33:35 +0300

    Refactor IAM configuration for GKE Workload Identity
    Updated IAM bindings for GKE Workload Identity and clarified comments.

--- k8s/gitops/iam.tf
+++ k8s/gitops/iam.tf
@@ -12,7 +12,6 @@ resource "google_secret_manager_secret_iam_member" "secret_access" {
 }
 
 # Bind K8s Service Account (KSA) to Google Service Account (GSA) ---
-# This is the critical Workload Identity step.
 resource "google_service_account_iam_member" "workload_identity_binding" {
   service_account_id = google_service_account.gke_workload_sa.name
   role               = "roles/iam.workloadIdentityUser"
@@ -21,7 +20,6 @@ resource "google_service_account_iam_member" "workload_identity_binding" {
   member = "serviceAccount:${var.project_id}.svc.id.goog[chris/chris-sa]"
 }
 
-# --- Dedicated Service Account for GKE Nodes (VMs) ---
 # Nodes only need permissions to write logs and pull images.
 resource "google_service_account" "gke_node_sa" {
   account_id   = "gke-node-sa"
